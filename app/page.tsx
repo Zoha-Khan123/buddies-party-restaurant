1707ce801eb2ee2fb7af4452293df6da99a1c868
Zoha-Khan123 <zohakhan11004@gmail.com>
2025-07-30 00:16:43 +0500

    tailwind-css-add

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-teaGreen">
+    <div className="bg-red">
       HELLO WORLD 
     </div>
   );
